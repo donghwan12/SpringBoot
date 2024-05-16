ca54b40919d7204f28375d6ed437899dfe5cf9c7
donghwan12 <yws078@naver.com>
2024-05-16 17:36:59 +0900

    .

--- 05_ORM_JPA/src/test/java/com/example/app/domain/repository/UserRepositoryTest.java
+++ 05_ORM_JPA/src/test/java/com/example/app/domain/repository/UserRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -32,6 +34,12 @@ class UserRepositoryTest {
                 .role("웹툰")
                 .build();
         userRepository.save(user);
+    }
+
+    @Test
+    public void jqplTest1(){
+        List<User> list= userRepository.selectByRole("ROLE_USER");
+        System.out.println(list);
     }
 
 }
