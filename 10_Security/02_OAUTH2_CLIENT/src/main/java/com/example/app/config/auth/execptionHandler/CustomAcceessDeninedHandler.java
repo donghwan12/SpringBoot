4ac2c5c74fb3a185f408160bbec0c2c2f33c4256
donghwan12 <yws078@naver.com>
2024-05-29 14:14:49 +0900

    .

---
+++ 10_Security/02_OAUTH2_CLIENT/src/main/java/com/example/app/config/auth/execptionHandler/CustomAcceessDeninedHandler.java
@@ -0,0 +1,16 @@
+package com.example.app.config.auth.execptionHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAcceessDeninedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        System.out.println("CustomAcceessDeninedHandler handle()..."+accessDeniedException.getMessage());
+    }
+}
